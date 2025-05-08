3b65af11444fbab7df6b095ee0a9181447900f28
Matt Clark <matt@mclarkdev.com>
2025-05-08 11:53:12 -0400

    Using new LibLog API for dynamic stream loading.

---
+++ src/main/java/com/mclarkdev/tools/libloggelf/lib/LibLogGELFLogWriter.java
@@ -0,0 +1,43 @@
+package com.mclarkdev.tools.libloggelf.lib;
+
+import java.net.URI;
+import java.net.UnknownHostException;
+
+import org.json.JSONObject;
+
+import com.mclarkdev.tools.liblog.lib.LibLogCachedLogWriter;
+import com.mclarkdev.tools.liblog.lib.LibLogMessage;
+
+/**
+ * LibLogGELF // LibLogGELFLogWriter
+ */
+public class LibLogGELFLogWriter extends LibLogCachedLogWriter {
+
+	public static String scheme() {
+		return "gelf";
+	}
+
+	public LibLogGELFLogWriter(URI uri) throws UnknownHostException {
+		super(uri, new LibLogGELFStream(uri));
+	}
+
+	@Override
+	public void write(LibLogMessage message) {
+
+		JSONObject obj = (new JSONObject())//
+				// .put("host", appName)//
+				.put("version", "0.1")//
+				.put("time", message.getTime())//
+				.put("stamp", message.getTimeStamp())//
+				.put("facility", message.getLoggedFacility())//
+				.put("message", message.getLoggedMessage())//
+				.put("throwable", message.getLoggedThrowableString());
+
+		if (debug) {
+			obj.put("className", message.getLoggedClassName());
+			obj.put("classLine", message.getLoggedLineNumber());
+		}
+
+		cache(obj.toString());
+	}
+}
